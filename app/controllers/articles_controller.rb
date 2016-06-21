451c9f9a840676c8671d83b10d5008fd044f7f10
Ignacio Ruben <ignacioruben7@gmail.com>
2016-06-21 18:07:20 -0500

    Añadiendo CRUD a articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,49 @@
 class ArticlesController < ApplicationController
+	
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@articles = Article.all.order("created_at DESC")
 	end
+
+	def show
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(post_params)
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update(post_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to root_path
+	end
+
+	private
+	def find_post
+		@article = Article.find(params[:id])
+	end
+
+	def post_params
+		params.require(:article).permit(:title,:body)
+	end 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def acerca
+  end
+
+  def privacidad
+  end
+
+  def terminos
+  end
+
+  def normas
+  end
+
+  def empleo
+  end
 end
