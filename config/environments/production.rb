0017a3f2fe61846b2e2147e6eb16b1429b84d068
Ignacio Ruben <ignacioruben7@gmail.com>
2016-06-29 19:37:09 -0500

    Commit 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   # Configurando Paperclip para subir imagenes a Amazon S3
 config.paperclip_defaults = {
   storage: :s3,
+  url: :s3_domain_url
   s3_credentials: {
-    url: ':s3_domain_url',
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
