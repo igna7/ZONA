af9a12a4b6a40f546f50f80f8bd4622d14c6bc57
Ignacio Ruben <ignacioruben7@gmail.com>
2016-06-29 19:39:10 -0500

    Fix error-1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,8 @@ Rails.application.configure do
   # Configurando Paperclip para subir imagenes a Amazon S3
 config.paperclip_defaults = {
   storage: :s3,
-  url: :s3_domain_url
+  :url => ":s3_domain_url",
+  :path => ":class/:id.:style.:extension",
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
