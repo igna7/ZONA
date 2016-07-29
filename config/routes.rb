726f11ac761f7123103df6a5848f914f95752e97
Ignacio Ruben <ignacioruben7@gmail.com>
2016-07-29 15:50:12 -0500

    bug 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: redirect('/')
 
   resources :categories
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
