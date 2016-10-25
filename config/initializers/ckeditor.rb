4dc7cf8a6c8fe893dacc20f3f744573d7c753b4f
Ignacio Ruben <ignacioruben7@gmail.com>
2016-10-25 14:49:18 -0500

    Fix bugs 1.0.10

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -4,9 +4,7 @@ Ckeditor.setup do |config|
   # Load and configure the ORM. Supports :active_record (default), :mongo_mapper and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = "//cdn.ckeditor.com/4.5.11/standard/ckeditor.js"
-  
+
   require "ckeditor/orm/active_record"
 
   # Allowed image file types for upload.
